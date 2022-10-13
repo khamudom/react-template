a64eba2f32ef18e0765ee472fd9aaa630267204b
khamudom <khamu@outlook.com>
2022-10-13 13:58:46 -0700

    add support for images and svgs

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import "./styles.css";
+import IMAGE from "./react.png";
+import LOGO from "./angry-svg.svg";
+
 export const App = () => {
-    return <div>App</div>;
+    return (
+        <>
+            <h1>App</h1>
+            <img src={IMAGE} alt="image" height={300} />
+            <img src={LOGO} alt="image" height={300} />
+        </>
+    );
 };
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -17,6 +17,18 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: "asset/inline",
+            },
         ],
     },
     output: {
