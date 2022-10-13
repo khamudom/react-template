0de475e3e38a1a92a3b14c3f04dd4fcc762490d5
khamudom <khamu@outlook.com>
2022-10-13 14:29:01 -0700

    config webpack to support mulitple environment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import LOGO from "./angry-svg.svg";
 export const App = () => {
     return (
         <>
-            <h1>App</h1>
+            <h1>
+                App {process.env.NODE_ENV} {process.env.name}
+            </h1>
             <img src={IMAGE} alt="image" height={300} />
             <img src={LOGO} alt="image" height={300} />
         </>
