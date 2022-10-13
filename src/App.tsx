db3a218f8c063a8626a8682c3b5e5ab0d522196a
khamudom <khamu@outlook.com>
2022-10-13 15:16:55 -0700

    add counter component and refresh plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import "./styles.css";
 import IMAGE from "./react.png";
 import LOGO from "./angry-svg.svg";
+import { ClickCounter } from "./ClickCounter";
 
 export const App = () => {
     return (
         <>
             <h1>
-                App {process.env.NODE_ENV} {process.env.name}
+                Hello React App {process.env.NODE_ENV} {process.env.name}
             </h1>
             <img src={IMAGE} alt="image" height={300} />
             <img src={LOGO} alt="image" height={300} />
+            <ClickCounter />
         </>
     );
 };
---
+++ src/ClickCounter.tsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export const ClickCounter = () => {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    setCount((c) => c + 1);
+                }}
+            >
+                Count: {count}
+            </button>
+        </div>
+    );
+};
